921145b00b565644ab32fb984f6031b12d75e4d5
Kristel EiTea <kristel.siilak@tptlive.ee>
2019-03-26 14:15:50 +0200

    first commit

---
+++ ILikeHashtags/HashtagFinder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Threading.Tasks;
+using InstaSharper.API;
+using InstaSharper.Classes;
+
+namespace ILikeHashtags
+{
+    internal class HashtagFinder : IDemoSample
+    {
+        private static readonly int _maxDescriptionLength = 20;
+
+        private readonly IInstaApi _instaApi;
+
+        public HashtagFinder(IInstaApi instaApi)
+        {
+            _instaApi = instaApi;
+        }
+
+        public async Task DoShow()
+        {
+
+            var tagFeed = await _instaApi.GetTagFeedAsync("quadcopter", PaginationParameters.MaxPagesToLoad(5));
+            if (tagFeed.Succeeded)
+            {
+                Console.WriteLine(
+                    $"Tag feed items (in {tagFeed.Value.MediaItemsCount} pages) [{currentUser.Value.UserName}]: {tagFeed.Value.Medias.Count}");
+                foreach (var media in tagFeed.Value.Medias)
+                    ConsoleUtils.PrintMedia("Tag feed", media, _maxDescriptionLength);
+            }
+        }
+    }
+}
